45d59d12aa2394aefc978321c432030da3f1ef49
RashSR <reinhold.schlager@gmx.de>
2019-05-24 23:23:49 +0200

    Added EnemyWalkTask. Snail can now Walk in a Pattern.

---
+++ src/hpps1/EnemyWalkTask.java
@@ -0,0 +1,55 @@
+package hpps1;
+import net.canarymod.Canary;
+import net.canarymod.logger.Logman;
+import net.canarymod.tasks.ServerTask;
+import com.pragprog.ahmine.ez.EZPlugin;
+import net.canarymod.api.world.position.Location;
+import net.canarymod.api.world.blocks.BlockType;
+public class EnemyWalkTask extends ServerTask{
+	private final EZPlugin plugin;
+	public static Logman logger;
+	private Enemy enemy;
+	private int count = 0;
+	public EnemyWalkTask(EZPlugin plugin, Enemy enemy) {
+        super(Canary.getServer(), 15, true);
+        logger = plugin.getLogman();
+        this.plugin = plugin;
+        this.enemy=enemy;
+    }
+
+    protected void killTask(){
+    	Canary.getServer().removeSynchronousTask(this);
+    }
+
+    public void run(){
+    	if(enemy instanceof Snail){
+    		Snail snail = (Snail)enemy;
+    		if(snail.getHealth()>0){
+    			Location loc=snail.getEnemyLocation();
+    			BlockType check=loc.getWorld().getBlockAt((int)loc.getX()+1, (int)loc.getY(), (int)loc.getZ()).getType(); //Blockcheck für alle Richtungen 
+    			if(check==BlockType.Air || check==BlockType.Mycelium){ 
+    				snail.changeEnemy(BlockType.Air);
+    				if(count<5){
+    					snail.move(Direction.POS_X);
+    				} else if(count<9){
+    					snail.move(Direction.NEG_Z);
+    				} else if(count<14){
+    					snail.move(Direction.NEG_X);
+    				} else{
+    					snail.move(Direction.POS_Z);
+    					if(count == 17){
+    						count = -1;
+    					}
+    				} 
+    			} else {
+    				logger.info("Snail ist gegen Block gelaufen.");
+    				snail.changeEnemy(BlockType.Air);
+    				killTask();
+    			}
+    		} else{
+    			killTask();
+    		}
+    	}
+    	count++;
+	}
+}
--- src/hpps1/Snail.java
+++ src/hpps1/Snail.java
@@ -1,6 +1,7 @@
 package hpps1;
 import net.canarymod.api.world.position.Location;
 import java.util.ArrayList;
+import net.canarymod.Canary;
 import net.canarymod.api.world.blocks.BlockType;
 
 public class Snail extends Enemy{
@@ -11,21 +12,35 @@ public class Snail extends Enemy{
 
 	protected static void initSnails(){
 		allSnails = new ArrayList<>();
-		allSnails.add(new Snail(HEALTH, new Location(-322, 79, 359), DAMAGE_GIVE, DAMAGE_TAKE));
+		allSnails.add(new Snail(HEALTH, new Location(-337, 79, 362), DAMAGE_GIVE, DAMAGE_TAKE));
 		for(Snail s : allSnails){
 			s.changeEnemy(BlockType.Mycelium);
+			s.walk();
 		}
 	}
 	protected Snail(int health, Location loc, int damageGive, int damageTake){
 		super(health, loc, damageGive, damageTake);
 	}
 	protected void walk(){
-		//TODO: ServerTask
+		Canary.getServer().addSynchronousTask(new EnemyWalkTask(this, this));
+	}
+	protected void move(Direction dir){
+		Location loc = getEnemyLocation();
+		if(dir==Direction.POS_X){
+			this.setEnemyLocation(new Location(loc.getX()+1, loc.getY(), loc.getZ()));
+		}else if(dir==Direction.NEG_X) {
+			this.setEnemyLocation(new Location(loc.getX()-1, loc.getY(), loc.getZ()));
+		}else if(dir==Direction.POS_Z){
+			this.setEnemyLocation(new Location(loc.getX(), loc.getY(), loc.getZ()+1));
+		}else {
+			this.setEnemyLocation(new Location(loc.getX(), loc.getY(), loc.getZ()-1));
+		}
+		this.changeEnemy(BlockType.Mycelium); 
 	}
 	protected void killEnemy(){
 		changeEnemy(BlockType.Air);
 	}
-	private void changeEnemy(BlockType bT){
+	protected void changeEnemy(BlockType bT){
 		this.getEnemyLocation().getWorld().setBlockAt(this.getEnemyLocation(), bT);
 	}
 	public void getHitBySpell(Spell spell, Spieler spieler){
@@ -33,6 +48,9 @@ public class Snail extends Enemy{
 			this.setHealth(this.getHealth()-this.getDamageTake());
 		} else if(spell == Spell.DOUBLEFLIPENDO){
 			this.setHealth(this.getHealth()-this.getDamageTake()*2);
+		}
+		if(this.getHealth()<0){
+			spieler.changeHousePoints(5);
 		}
 	}
 	protected static Snail checkIfSnail(Location loc){
